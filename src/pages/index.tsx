52deb1856dd30cdb56df090f0d5fc3857f02c283
ezza022 <alfirman.pahlepi@gmail.com>
2021-11-01 23:56:16 +0700

    feat: add layout add navigations

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,14 @@
+import Layout from '@/components/Layout'
 import type { NextPage } from 'next'
 
 
 const Home: NextPage = () => {
   return (
-    <main>
-      <div className='h-[100vh] flex flex-col justify-center align-middle text-center'>
-        <h1 className='text-[72px] bg-clip-text text-transparent bg-gradient-to-r from-[#fcc] to-[#bada55] font-extrabold'>
-          kencang parah nextjs 12
-        </h1>
-        <h2 className='text-2xl max-w-md mx-auto'>
-          A Next.js Boilerplate with TypeScript, Tailwind CSS and testing
-          suite enabled
-        </h2>
-      </div>
-    </main>
+    <Layout>
+      <main>
+        d
+      </main>
+    </Layout>
 
   )
 }
