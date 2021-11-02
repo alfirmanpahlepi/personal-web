71147baf1ff7dee12e0cef7b6cc182a5130f6a0a
ezza022 <alfirman.pahlepi@gmail.com>
2021-11-02 21:37:10 +0700

    feat: add page portfolio and contact

---
+++ src/pages/contact.tsx
@@ -0,0 +1,12 @@
+import Layout from "@/components/Layout"
+import { NextPage } from "next"
+
+const Contact:NextPage = () => {
+  return (
+    <Layout>
+      contact
+    </Layout>
+  )
+}
+
+export default Contact
---
+++ src/pages/portfolio.tsx
@@ -0,0 +1,12 @@
+import Layout from "@/components/Layout"
+import { NextPage } from "next"
+
+const Portfolio:NextPage = () => {
+  return (
+    <Layout>
+      portfolio
+    </Layout>
+  )
+}
+
+export default Portfolio
