400d16a1234da4b7674b5363c5d106328d0c02a4
ezza022 <alfirman.pahlepi@gmail.com>
2021-11-04 19:15:05 +0700

    fix: deactive autocroll on / page

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,18 +2,20 @@ import { ReactNode, useEffect, useRef } from 'react';
 import HeroImage from './HeroImage';
 import Navigations from './Navigations';
 import { motion } from "framer-motion"
+import { useRouter } from 'next/router';
 
 interface LayoutProps {
   children: ReactNode;
 }
 
 export default function Layout({ children }: LayoutProps) {
+  const { asPath } = useRouter();
   const theme: string = 'linear-gradient(45deg,#405de6,#5851db,#833ab4,#c13584,#e1306c,#fd1d1d)';
-  const mainRef = useRef<HTMLDivElement>(null)
-  const layoutRef = useRef<HTMLDivElement>(null)
+  const mainRef = useRef<HTMLDivElement>(null);
+  const layoutRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    if (window.matchMedia('(max-width: 1280px)')) {
+    if (window.matchMedia('(max-width: 1280px)') && asPath !== "/") {
       const layoutScrollHeight: number = layoutRef.current?.scrollHeight || 0;
       const mainScrollHeight: number = mainRef.current?.scrollHeight || 0;
       window.scroll({
